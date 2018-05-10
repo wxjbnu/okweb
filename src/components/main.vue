06ce1760793c3bbabe17726f753ff1bf714e548f
wxj <139439432@qq.com>
2018-05-11 00:44:15 +0800

    0511

---
+++ src/components/main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main123',
+  data () {
+    return {
+      
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
